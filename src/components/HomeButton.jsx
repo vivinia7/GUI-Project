28aea03ed1089d3dbeac80b4c87bab5542f8e427
vivinia7 <127296683+vivinia7@users.noreply.github.com>
2023-03-23 21:48:29 +0000

    Update HomeButton.jsx

--- src/components/HomeButton.jsx
+++ src/components/HomeButton.jsx
@@ -5,7 +5,7 @@ function HomeButton({onClick}) {
   return (
     <div className="cursor-pointer text-white flex justify-start my-5 transition ease-in-out hover:scale-125">
         <UilArrowLeft/>
-        <button onClick={onClick}>Forecast</button>
+        <button onClick={onClick} className="transition-all delay-75 ease-in-out">Forecast</button>
     </div>
     
   )
