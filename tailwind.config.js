bfc2e3d58c628dc4a071dff6fedb7f859abd3489
vivinia7 <127296683+vivinia7@users.noreply.github.com>
2023-03-23 17:26:11 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+      'cold' : "url('https://unsplash.com/photos/iYAF7dhw7yI')"
+    }
+    },
+  },
+  plugins: [],
+};
