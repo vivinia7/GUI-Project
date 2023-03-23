740ead8c65b9bc40f004ef62b9d5f997747f3562
vivinia7 <127296683+vivinia7@users.noreply.github.com>
2023-03-23 22:56:37 +0000

    Update and rename src/components/TopButtons.jsx to src/FactButtons.jsx

--- src/FactButtons.jsx
+++ src/FactButtons.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { UilArrowRight } from '@iconscout/react-unicons'
 
-function TopButtons({onClick}) {
-
+function FactButtons({onClick}) {
+  {/* The function to enable the onClick event */}
   return (
     <div className="cursor-pointer text-white flex justify-end my-5 transition ease-in-out hover:scale-125">
       <button onClick={onClick} className="transition-all delay-75 ease-in-out"> Fun Fact</button>
@@ -13,4 +13,4 @@ function TopButtons({onClick}) {
   )
 }
 
-export default TopButtons
+export default FactButtons
